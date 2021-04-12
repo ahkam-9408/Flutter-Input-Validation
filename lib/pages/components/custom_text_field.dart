1c3dc2c372b11b7eef2c52ee4c252360733a7fda
ahkam-9408 <it18098174@my.sliit.lk>
2021-04-12 22:17:11 +0530

    Custom sized box added and custorm text field modified

--- lib/pages/components/custom_text_field.dart
+++ lib/pages/components/custom_text_field.dart
@@ -4,16 +4,23 @@ class CustomTextField extends StatelessWidget {
   final String labelText;
   final String hintText;
 
-  const CustomTextField({this.labelText,this.hintText});
+  final String errTxt;
+
+  const CustomTextField({this.labelText, this.hintText, this.errTxt});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      obscureText: true,
       decoration: InputDecoration(
+          errorText: errTxt,
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-          ),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              borderSide: errTxt != null
+                  ? BorderSide(color: Colors.red, width: 5)
+                  : null),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.red)),
           labelText: labelText,
           hintText: hintText),
     );
--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:input_validation_excercise/pages/components/custom_sized_box.dart';
 import 'package:input_validation_excercise/pages/components/custom_text_field.dart';
 
 class FormPage extends StatefulWidget {
@@ -9,13 +10,44 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  String errorFirstName = "Error";
+  String errorLastName = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: Container(
-            padding: EdgeInsets.all(20),
-            child: CustomTextField(hintText: "Enter your email here",labelText: "Email",)
-      ),
+      body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Student Registration",
+                style: TextStyle(
+                  color: Color(0xff48a572),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                hintText: "Enter your first name here",
+                labelText: "First Name",
+                errTxt: errorFirstName,
+              ),
+              CustomSizedBox(),
+              CustomTextField(
+                hintText: "Enter your last name here",
+                labelText: "Last Name",
+                errTxt: errorLastName,
+              ),
+              CustomSizedBox(),
+            ],
+          )),
     );
   }
 }
