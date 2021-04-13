55550bc997e6b75b301d1029c8a01ecf191858d7
ahkam-9408 <it18098174@my.sliit.lk>
2021-04-13 15:14:20 +0530

    Some Input validation added

--- lib/pages/components/custom_text_field.dart
+++ lib/pages/components/custom_text_field.dart
@@ -1,28 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:regexpattern/regexpattern.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String labelText;
   final String hintText;
+  final TextInputType keyboardType;
+  final Pattern pattern;
+  final bool isEnabled;
+  final String customReg;
+  final String customReg2;
+  final int maxLength;
 
   final String errTxt;
 
-  const CustomTextField({this.labelText, this.hintText, this.errTxt});
+  CustomTextField(
+      {@required this.labelText,
+      @required this.hintText,
+      this.errTxt,
+      this.pattern,
+      this.customReg,
+      this.customReg2,
+      @required this.keyboardType,
+      this.isEnabled = true,
+      this.maxLength});
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  String errorText = "";
+
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
+      keyboardType: widget.keyboardType,
+      onChanged: onChange,
+      enabled: widget.isEnabled,
+      maxLength: widget.maxLength,
       decoration: InputDecoration(
-          errorText: errTxt,
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-              borderSide: errTxt != null
-                  ? BorderSide(color: Colors.red, width: 5)
-                  : null),
-          errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide(color: Colors.red)),
-          labelText: labelText,
-          hintText: hintText),
+        errorText: errorText == "" ? null : errorText,
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12))),
+        errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: Colors.red)),
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+      ),
     );
+  }
+
+  void onChange(value) {
+    if (widget.customReg == "TryOtherPattern") {
+      if (RegVal.hasMatch(controller.text, widget.pattern)) {
+        setErrorState("");
+      } else {
+        setErrorState(widget.errTxt);
+      }
+    } else {
+      if (widget.customReg2 != null) {
+        if (RegExp(widget.customReg2).hasMatch(controller.text) ||
+            RegExp(widget.customReg).hasMatch(controller.text)) {
+          setErrorState("");
+        } else {
+          setErrorState(widget.errTxt);
+        }
+      } else {
+        if (RegExp(widget.customReg).hasMatch(controller.text)) {
+          setErrorState("");
+        } else {
+          setErrorState(widget.errTxt);
+        }
+      }
+    }
+  }
+
+  void setErrorState(String eText) {
+    setState(() {
+      errorText = eText;
+    });
   }
 }
--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:input_validation_excercise/pages/components/custom_sized_box.dart';
 import 'package:input_validation_excercise/pages/components/custom_text_field.dart';
+import 'package:regexpattern/regexpattern.dart';
 
 class FormPage extends StatefulWidget {
   FormPage({Key key}) : super(key: key);
@@ -10,44 +11,111 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
-  String errorFirstName = "Error";
-  String errorLastName = "";
+  String errorFirstName = "Please Enter Valid First Name";
+  String errorLastName = "Please Enter Valid Last Name";
+  String errorNIC = "Please Enter Valid NIC Number";
+  String errorMobileNumber = "Please Enter Valid Mobile Number";
+  String errorAddress = "Please Enter Address";
+
+  final firstName = TextEditingController();
+  final lastName = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                "Student Registration",
-                style: TextStyle(
-                  color: Color(0xff48a572),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+      body: SingleChildScrollView(
+        child: Container(
+            width: screenWidth,
+            height: screenHeight,
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomTextField(
-                hintText: "Enter your first name here",
-                labelText: "First Name",
-                errTxt: errorFirstName,
-              ),
-              CustomSizedBox(),
-              CustomTextField(
-                hintText: "Enter your last name here",
-                labelText: "Last Name",
-                errTxt: errorLastName,
-              ),
-              CustomSizedBox(),
-            ],
-          )),
+                Text(
+                  "Student Registration",
+                  style: TextStyle(
+                    color: Color(0xff48a572),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                  hintText: "Enter your first name here",
+                  labelText: "First Name",
+                  errTxt: errorFirstName,
+                  keyboardType: TextInputType.name,
+                  customReg: r"^(?![\s.]+$)[a-zA-Z\s.]*$",
+                ),
+                CustomSizedBox(),
+                CustomTextField(
+                    hintText: "Enter your last name here",
+                    labelText: "Last Name",
+                    errTxt: errorLastName,
+                    keyboardType: TextInputType.name,
+                    customReg: r"^(?![\s.]+$)[a-zA-Z\s.]*$"),
+                CustomSizedBox(),
+                CustomTextField(
+                    hintText: "Enter your birthday here",
+                    labelText: "Birthday",
+                    errTxt: errorLastName,
+                    keyboardType: TextInputType.datetime,
+                    pattern: RegexPattern.basicDateTime),
+                CustomSizedBox(),
+                CustomTextField(
+                  hintText: "Enter your age here",
+                  labelText: "Age",
+                  errTxt: errorLastName,
+                  keyboardType: TextInputType.name,
+                  pattern: RegexPattern.alphabetOnly,
+                  isEnabled: false,
+                ),
+                CustomSizedBox(),
+                CustomTextField(
+                  hintText: "Enter your Number here",
+                  labelText: "NIC",
+                  errTxt: errorNIC,
+                  keyboardType: TextInputType.text,
+                  customReg: r"^[0-9]{9}[A-Z]{1}$",
+                  customReg2: r"^[0-9]{12}$",
+                  maxLength: 12,
+                ),
+                CustomSizedBox(),
+                CustomTextField(
+                    hintText: "Enter your mobile number here",
+                    labelText: "Mobile Number",
+                    errTxt: errorMobileNumber,
+                    keyboardType: TextInputType.phone,
+                    maxLength:10,
+                    pattern: RegexPattern.numericOnly,
+                    customReg: "TryOtherPattern",
+                ),
+                CustomSizedBox(),
+                CustomTextField(
+                    hintText: "Enter your address here",
+                    labelText: "Address",
+                    errTxt: errorAddress,
+                    keyboardType: TextInputType.streetAddress,
+                    customReg: r"^[a-zA-Z0-9\s,.'-]{3,}$",
+                ),
+                CustomSizedBox(),
+                CustomTextField(
+                    hintText: "Enter your address here",
+                    labelText: "School or University",
+                    errTxt: errorAddress,
+                    keyboardType: TextInputType.streetAddress,
+                    customReg: r"^[a-zA-Z0-9\s,.'-]{3,}$",
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
